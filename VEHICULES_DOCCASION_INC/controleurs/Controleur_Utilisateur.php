e3d7411a33bed35718afcbb93d8ca1b1cc325e75
apauyo <paua_11@hotmail.com>
2021-04-14 21:29:56 -0400

    Ajout controleur Utilisateur

---
+++ VEHICULES_DOCCASION_INC/controleurs/Controleur_Utilisateur.php
@@ -0,0 +1,41 @@
+<?php
+	class Controleur_Utilisateur extends BaseControleur {
+	
+		//la fonction qui sera appelée par le routeur
+		public function traite(array $params) {
+			
+			$this->afficheVue("Head");
+			$this->afficheVue("Header");
+			
+			if (isset($params["action"])) {
+				// Modèle et vue vides par défaut
+				$data = array();
+				
+				// Switch en fonction de l'action qui nous est envoyée
+				// Ce switch détermine la vue $vue et obtient le modèle $data
+				switch($params["action"]) {
+
+
+
+					
+					
+				}			
+			} else {
+				// Action par défaut
+				$vue = "VoitureListe";		
+				
+				
+                $modeleVoiture = new Modele_Voiture();
+				$data = $modeleVoiture->obtenirTous();
+				//var_dump($data);
+				$this->afficheVue($vue,$data);
+				 /*$data = array_merge($nbProducts, $allProducts);		
+                $vue = "ProductList";		
+				$this->showView($vue, $data);
+				$vue = "ProductListEnd";		
+                $this->showView($vue); */
+			}
+			$this->afficheVue("Footer");
+		}
+	}
+?>
