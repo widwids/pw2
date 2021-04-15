f97055f8588259f50033deede2fe38b55684be58
apauyo <paua_11@hotmail.com>
2021-04-15 15:02:16 -0400

    Ajout dans l'affichage utilisateur

--- VEHICULES_DOCCASION_INC/vues/AfficheUtilisateur.php
+++ VEHICULES_DOCCASION_INC/vues/AfficheUtilisateur.php
@@ -1,1 +1,15 @@
-<h1>Utilisateur : <?= $data["utilisateur"] -> getPrenom() . " " . $data["utilisateur"] -> getNom() ?></h1>
+<?php $utilisateur = $data["utilisateur"] ?>
+
+<h1>Utilisateur : <?= $utilisateur -> getPrenom() . " " . $utilisateur -> getNom() ?></h1>
+
+<ul>
+    <li>Date de naissance : <?= $utilisateur -> getDateNaissance() ?></li>
+    <li>Adresse : <?= $utilisateur -> getAdresse() ?></li>
+    <li>Code postal : <?= $utilisateur -> getCodePostal() ?></li>
+    <li>Téléphone : <?= $utilisateur -> getTelephone() ?></li>
+    <li>Cellulaire : <?= $utilisateur -> getCellulaire() ?></li>
+    <li>Courriel : <?= $utilisateur -> getCourriel() ?></li>
+    <li>Ville : </li>
+    <li>Province : </li>
+    <li>Pays : </li>
+</ul>
