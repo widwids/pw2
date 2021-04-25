950576abfc2d4ca484e7579e09e7a87731843275
Yuliya33 <yuliya.polyakova1131@gmail.com>
2021-04-25 14:25:59 -0400

    front-end du côté admin

---
+++ VEHICULES_DOCCASION_INC/vues/HeaderAdmin.php
@@ -0,0 +1,36 @@
+<header class="header-admin" data-component="Header">
+	<picture>
+		<img src="./assets/images/YVMA_logo_gold.svg" alt="">
+		<a href="#" class="brand_text">YVMA</a>
+	</picture>
+	<ul class="menu-principale">
+		<li class="parent-menu-deroulant"><a href="#">Gestion de véhicule</a>
+			<ul class="menu-deroulant">
+				<li><a href="index.php?Admin&action=FormulaireAjouterVoiture">Ajouter un véhicule</a></li>
+				<li><a href="">Liste de véhicule</a></li>
+			</ul>
+		</li>
+		<li class="parent-menu-deroulant"><a href="#">Gestion d’utilisateur</a>
+			<ul class="menu-deroulant">
+				<li><a href="">Mon compte</a></li>
+				<li><a href="">Fermer la session</a></li>
+				<li><a href="">Mon compte</a></li>
+				<li><a href="">Fermer la session</a></li>
+				<li><a href="">Mon compte</a></li>
+				<li><a href="">Fermer la session</a></li>
+			</ul>
+		</li>
+		<li class="parent-menu-deroulant"><a href="#">John Doe(administrateur)</a>
+			<ul class="menu-deroulant">
+				<li><a href="">Mon compte</a></li>
+				<li><a href="">Fermer la session</a></li>
+			</ul>
+		</li>
+		<li><a href="#">FR</a></li>
+	</ul>
+</header>
+<div style="height: 100%">
+
+</div>
+
+
