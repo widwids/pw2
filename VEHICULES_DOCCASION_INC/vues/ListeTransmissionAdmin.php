6468175f7fcb20b1b2b350b25e14cc579182c867
Yuliya33 <yuliya.polyakova1131@gmail.com>
2021-04-26 19:35:08 -0400

    liste de transmission (admin)

---
+++ VEHICULES_DOCCASION_INC/vues/ListeTransmissionAdmin.php
@@ -0,0 +1,33 @@
+
+<section class="yu-section">
+
+    <div class="yu-table-transmission">
+        <a class="yu-btn-ajouter" href="index.php?Utilisateur&action=FormulaireAjouterTransmission">Ajouter transmission</a>
+    </div>
+
+    <table class="yu-table yu-table-transmission">
+               
+        <tr>
+            <th>Nom du transmission en français</th>
+            <th>Nom du transmission en anglais</th>
+            <th>Visibilité</th>
+            <th>Actions</th>
+        </tr>
+
+        <tr>
+            <td>TEST</td>
+            <td>TEST</td>
+            <td>OUI</td>
+            <td><button class="yu-btn-modifier yu-btn">Modifier</button><button class="yu-btn-supprimer yu-btn">Supprimer</button></td>
+        </tr>
+
+        <tr>
+            <td>TEST</td>
+            <td>TEST</td>
+            <td>NON</td>
+            <td><button class="yu-btn-modifier yu-btn">Modifier</button><button class="yu-btn-supprimer yu-btn">Supprimer</button></td>
+        </tr>
+
+    </table>
+
+</section>
