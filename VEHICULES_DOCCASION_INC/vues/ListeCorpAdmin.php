e10f2393f410b6f798678635dd8fe0f2ca069012
Yuliya33 <yuliya.polyakova1131@gmail.com>
2021-04-26 19:34:05 -0400

    liste de corps (admin)

---
+++ VEHICULES_DOCCASION_INC/vues/ListeCorpAdmin.php
@@ -0,0 +1,33 @@
+
+<section class="yu-section">
+
+    <div class="yu-table-corp">
+        <a class="yu-btn-ajouter" href="index.php?Utilisateur&action=FormulaireAjouterCorp">Ajouter corp</a>
+    </div>
+
+    <table class="yu-table yu-table-corp">
+               
+        <tr>
+            <th>Nom du corp en français</th>
+            <th>Nom du corp en anglais</th>
+            <th>Visibilité</th>
+            <th>Actions</th>
+        </tr>
+
+        <tr>
+            <td>TEST</td>
+            <td>TEST</td>
+            <td>OUI</td>
+            <td><button class="yu-btn-modifier yu-btn">Modifier</button><button class="yu-btn-supprimer yu-btn">Supprimer</button></td>
+        </tr>
+
+        <tr>
+            <td>TEST</td>
+            <td>TEST</td>
+            <td>NON</td>
+            <td><button class="yu-btn-modifier yu-btn">Modifier</button><button class="yu-btn-supprimer yu-btn">Supprimer</button></td>
+        </tr>
+
+    </table>
+
+</section>
