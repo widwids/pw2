3f2c206310bf89b14d0d131882776d2c16fdf935
Mathieu D <duchesne.math@gmail.com>
2021-04-27 19:49:39 -0400

    update navbar
    - Ajout hamburger
    - Changement de fonte (overpass 100, 400)
    - Navbar maintenant responsive
    - Changement background
    - Plusieurs effets CSS pour un meilleur look (navbar)
    - Mise en commentaire de toute la section *HEADER** dans style.css

--- VEHICULES_DOCCASION_INC/vues/Head.php
+++ VEHICULES_DOCCASION_INC/vues/Head.php
@@ -13,10 +13,12 @@
 	<script type="text/javascript" src="assets/scripts/Voiture.js"></script>
 	<script type="text/javascript" src="assets/scripts/classMapping.js"></script>
 	<script type="text/javascript" src="assets/scripts/script.js" defer></script>
+	<script type="text/javascript" src="assets/scripts/nav.js" defer></script>
 
 	<!-- Google Fonts -->
 	<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Marcellus+SC&family=Montserrat:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Marcellus+SC&family=Overpass:wght@100;400&display=swap" rel="stylesheet"> 
 
 </head>
 <body>
