bf9d4a90e945f8358d5127e03ed542285d111721
Yuliya33 <yuliya.polyakova1131@gmail.com>
2021-04-27 22:50:11 -0400

    formulaire ajoute voiture prêt pour les tests

--- VEHICULES_DOCCASION_INC/vues/FormulaireAjouterVoiture.php
+++ VEHICULES_DOCCASION_INC/vues/FormulaireAjouterVoiture.php
@@ -1,4 +1,4 @@
-<form action="index.php?Admin&action=FormulaireAjouterVoiture" method="post" class="yu-formulaire">
+<form action="index.php?Voiture&action=ajoutVoiture" method="post" class="yu-formulaire">
 	<div>
 		<label for="noSerie">№ Série</label>
 		<input type="text" name="noSerie" value="">
@@ -31,34 +31,64 @@
 		<label for="groupeMPid">Group MP</label>
 		<select name="groupeMPid" id="groupeMPid">
 			<option value="">Sélectionnez un groupe MP</option>
+				<?php foreach($data["motopropulseur"] as $groupeMP) { ?>
+
+					<option value="<?= $groupeMP["idMotopro"] ?>"><?= $groupeMP["nomMotopro"]?></option>
+
+				<?php }?>
 		</select>
 	</div>
 		<label for="corpsId">Corps</label>
 		<select name="corpsId" id="corpsId">
 			<option value="">Sélectionnez un corps</option>
+				<?php foreach($data["corps"] as $corp) { ?>
+
+					<option value="<?= $corp["idCorps"] ?>"><?= $corp["nomCorpsFR"]?></option>
+
+				<?php }?>
 		</select>
 	<div>
 		<label for="carburantId">Carburant</label>
-		<select name="carburantId" id="carburantId">
+		<select name="carburanstsId" id="carburantId">
 			<option value="">Sélectionnez un carburant</option>
+				<?php foreach($data["carburant"] as $carburant) { ?>
+
+					<option value="<?= $carburant["idCarburant"] ?>"><?= $carburant["typeCarburantFR"]?></option>
+
+				<?php }?>
 		</select>
 	</div>
 	<div>
 		<label for="modeleId">Modèle</label>
 		<select name="modeleId" id="modeleId">
 			<option value="">Sélectionnez un modèle</option>
+				<?php foreach($data["modele"] as $modele) { ?>
+
+					<option value="<?= $modele["nomModele"] ?>"><?= $modele["nomModele"]?></option>
+
+				<?php }?>
 		</select>
 	</div>
 	<div>
 		<label for="transmissionId">Transmission</label>
 		<select name="transmissionId" id="transmissionId">
 			<option value="">Sélectionnez une transmission</option>
+				<?php foreach($data["transmission"] as $transmission) { ?>
+
+					<option value="<?= $transmission["idTransmission"] ?>"><?= $transmission["nomTransmissionFR"]?></option>
+
+				<?php }?>
 		</select>
 	</div>
 	<div>
 		<label for="anneeId">Année</label>
 		<select name="anneeId" id="anneeId">
 			<option value="">Sélectionnez une année</option>
+				<?php foreach($data["annee"] as $annee) { ?>
+
+					<option value="<?= $annee["annee"] ?>"><?= $annee["annee"]?></option>
+
+				<?php }?>
 		</select>
 	</div>
 	<div>
