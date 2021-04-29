9b3eccf5b973459d67c854c2d5f50d7fa465d885
Mathieu D <duchesne.math@gmail.com>
2021-04-29 15:06:16 -0400

    structure de base swipper
    changement initiaux:
    css, HTML, JS de base

--- VEHICULES_DOCCASION_INC/vues/Head.php
+++ VEHICULES_DOCCASION_INC/vues/Head.php
@@ -4,17 +4,21 @@
 	<title>YVMA.inc</title>
 
 	<!-- Styles -->
+	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 	<link rel="stylesheet" href="assets/styles/styles.css">
 	<link rel="stylesheet" href="assets/styles/header.css">
 	<link rel="stylesheet" href="assets/styles/footer.css">
 	<link rel="stylesheet" href="assets/styles/admin.css">
 	<link rel="stylesheet" href="assets/styles/connexion.css">
+	<link rel="stylesheet" href="assets/styles/detailVoiture.css">
 	
 	<!-- Script -->
 	<script type="text/javascript" src="assets/scripts/Voiture.js"></script>
 	<script type="text/javascript" src="assets/scripts/classMapping.js"></script>
 	<script type="text/javascript" src="assets/scripts/connexion.js"></script>
-
+	<script type="text/javascript" src="assets/scripts/detailVoiture.js"></script>
+	
+	<script src="https://unpkg.com/swiper/swiper-bundle.min.js" defer></script>
 	<script type="text/javascript" src="assets/scripts/script.js" defer></script>
 	<script type="text/javascript" src="assets/scripts/nav.js" defer></script>
 
