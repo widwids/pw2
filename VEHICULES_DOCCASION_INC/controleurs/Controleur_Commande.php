786b2f5275277e782e0eacbeec918b678a2efef8
apauyo <paua_11@hotmail.com>
2021-04-30 13:47:31 -0400

    Ajouts Controleur_Commande

--- VEHICULES_DOCCASION_INC/controleurs/Controleur_Commande.php
+++ VEHICULES_DOCCASION_INC/controleurs/Controleur_Commande.php
@@ -1,52 +1,82 @@
 <?php
 	class Controleur_Commande extends BaseControleur {		
         public function traite(array $params) {
+            $data = array();
 
-            $modeleCommandes = new Modele_Commande();
+            //Modèle pour les commandes
+            $modeleCommande = new Modele_Commande();
             
-			$this->afficheVue("Head");
-			//$this->afficheVue("Header");
-			
-			if (isset($params["action"])) {
-				// Modèle et vue vides par défaut
-				$data = array();
-				
-				// Switch en fonction de l'action qui nous est envoyée
-				// Ce switch détermine la vue $vue et obtient le modèle $data
-				switch($params["action"]) {
-                
-                    case "afficheCommandes":
-					
-					    //affiche toutes les commandes ////
-						
-						
-						$commandes = $modeleCommandes->obtenirCommandes();
-						var_dump($commandes);
-						/////////////////////
-						//$this->afficheVue($vue,$data); 
+			$this -> afficheVue("Head");
+			isset($_SESSION["employe"]) || isset($_SESSION["admin"]) ?
+                $this -> afficheVue("HeaderAdmin") : $this -> afficheVue("Header");
 
-					break;
-                    case "afficheFactureCommande":
-                        if (isset($params["idCommande"])) {
-                            
-                            $modeleCommandes = new Modele_Commande();
-                            //affiche une Facture d'une commande donnée
-                            $facture = $modeleCommandes->obtenirFacture($params["idCommande"]);
-                            var_dump($facture);
-                            //affiche une commande donnée
-                            $commande = $modeleCommandes->obtenirCommande($params["idCommande"]);
-                            var_dump($commande);
-                            /////////////////////
-                            //$this->afficheVue($vue,$data); 
-                        } else {													
-                            echo "ERROR PARAMS";
-                        } 
-					break;
-				}
-            }else {
+            if(isset($params["action"])) {
+				$action = $params["action"]; 
+            } else {
+                //Commande par défaut
+                $action = "afficheCommande";
+            }
 
-				
-			}
+            // Switch en fonction de l'action qui nous est envoyée
+            // Ce switch détermine la vue $vue et obtient le modèle $data
+            switch($action) {
+                case "afficheCommandes":
+                    //Affiche toutes les commandes
+                    if (isset($_SESSION["employe"]) || isset($_SESSION["admin"])) {
+                        $vue = "ListeCommandes";
+                        $data["commandes"] = $modeleCommande -> obtenirCommandes();
+                    
+                        $this -> afficheVue($vue, $data);
+                    } else {
+                        //Redirection vers le formulaire d'authentification
+                        header("Location: index.php?Utilisateur&action=connexion");
+                    }
+                    break;
+                case "afficheCommande":
+                    //Affiche une commande spécifique
+                    if (isset($params["idCommande"])) {
+                        //Affiche une commande donnée
+                        $vue = "Commande";
+                        $data["commande"] = $modeleCommande -> obtenirCommande($params["idCommande"]);
+
+                        $this -> afficheVue($vue, $data); 
+                    } else {													
+                        $this -> afficheVue("Page404");
+                    } 
+                    break;
+                case "afficheFactures":
+                    //Affiche toutes les factures
+                    if (isset($_SESSION["employe"]) || isset($_SESSION["admin"])) {
+                        $vue = "ListeFactures";
+                        $data["factures"] = $modeleCommande -> obtenirFactures();
+                    
+                        $this -> afficheVue($vue, $data);
+                    } else {
+                        //Redirection vers le formulaire d'authentification
+                        header("Location: index.php?Utilisateur&action=connexion");
+                    }
+                    break;
+                case "afficheFacture":
+                    //Affiche une facture donnée
+                    if (isset($params["idCommande"])) {
+                        //Affiche une Facture d'une commande donnée
+                        $vue = "Facture";
+                        $data["facture"] = $modeleCommande -> obtenirFacture($params["idCommande"]);
+
+                        $this -> afficheVue($vue, $data);
+                    } else {													
+                        $this -> afficheVue("Page404");
+                    }
+                    break;
+                case "afficheModePaiement":
+                    $vue = "ListeModePaiement";
+                    $data["modePaiement"] = $modeleCommande -> obtenir_tous("modePaiement");
+
+                    $this -> afficheVue($vue, $data);
+                    break;
+                default:
+                    $this -> afficheVue("Page404");
+            }
 
             $this->afficheVue("Footer");
         }
