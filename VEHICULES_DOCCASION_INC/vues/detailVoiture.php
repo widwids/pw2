f9af29fe4d90ca69ac5810eb5c5e0f10a9affc4d
Mathieu D <duchesne.math@gmail.com>
2021-04-30 21:49:40 -0400

    section détails et petit redesign du caroussel
    - section caroussel refaite
    - section details refaite pour un responsive plus facile à atteindre

--- VEHICULES_DOCCASION_INC/vues/detailVoiture.php
+++ VEHICULES_DOCCASION_INC/vues/detailVoiture.php
@@ -19,26 +19,35 @@
     <div class="swiper-button-prev"></div>
 </div>
 
-
-<div class="details">
-    <h1 class="titreDetails">Détails</h1>
-    <ul>
-        <li>Marque: Ferrari</li>
-        <li>Modèle: La ferrari</li>
-        <li>Année: 2013</li>
-        <li>Couleur: Rouge</li>
-        <li>Kilométrage: 18 850Km</li>
-        <li>Carburant: Essence Standard</li>
-        <li>Type de véhicule: Supercar</li>
-        <li class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Aenean et dolor et libero egestas porttitor et maximus nisl. 
-            Suspendisse vitae leo ut sapien dapibus dapibus a ut sem.
-        </li>
-    </ul>
-</div>
-
 <div class="nomMarque">
     <h1>Ferrari</h1>
     <h1>LA FERRARI</h1>
 </div>
+
+<div class="details_container">
+    <h1 class="titreDetails">Détails</h1>
+    <div class="details">
+        <div>
+            <ul>
+                <li>Marque: Ferrari</li>
+                <li>Modèle: La ferrari</li>
+                <li>Année: 2013</li>
+                <li>Couleur: Rouge</li>
+                <li>Kilométrage: 18 850Km</li>
+                <li>Carburant: Essence Standard</li>
+                <li>Type de véhicule: Supercar</li>
+            </ul>
+        </div>
+        <div class="description">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Aenean et dolor et libero egestas porttitor et maximus nisl. 
+                Suspendisse vitae leo ut sapien dapibus dapibus a ut sem.</p>
+        </div>
+    </div>
+
+    <div class="reservation">
+        <button class="reserver" data-js-reserver>Réservez ce véhicule</button>
+    </div>
+</div>
+
 
