3d4edb6030db9b9dd43ea936df04369dc5048f9c
Mathieu D <duchesne.math@gmail.com>
2021-05-01 21:45:03 -0400

    Update Footer.php

--- VEHICULES_DOCCASION_INC/vues/Footer.php
+++ VEHICULES_DOCCASION_INC/vues/Footer.php
@@ -13,7 +13,7 @@
 
         <div class="map">
             <h3>Google Map</h3>        
-            <img class="location" src="assets/logo_icones/location.png">
+            <img class="location" src="assets/images/location.png">
         </div>
 
         <div class="site_map">
