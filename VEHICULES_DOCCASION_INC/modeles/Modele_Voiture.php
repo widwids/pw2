8b67e21518c60deabb364e0b2c7fe4f97e24fe2d
hamaidi <82060880+mohammedHamaidi@users.noreply.github.com>
2021-05-10 11:02:52 -0400

    MAJ ControVoituAJAX

--- VEHICULES_DOCCASION_INC/modeles/Modele_Voiture.php
+++ VEHICULES_DOCCASION_INC/modeles/Modele_Voiture.php
@@ -385,61 +385,15 @@
 				return false;
 		}
 
-
-		/* function chargerImage($image_nom){
-
-			$target_dir = "images/livres/";
-			$target_file = $target_dir . basename($_FILES["image"]["name"]);
-	
-			$uploadOk = 1;
-			$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-	
-			$n_target_file = $target_dir . $image_nom . "." . $imageFileType;
-	
-			// Check if image file is a actual image or fake image
-			if(isset($_POST["bouton_ajouter"])) {
-				$check = getimagesize($_FILES["image"]["tmp_name"]);
-				if($check !== false) {
-					echo "File is an image - " . $check["mime"] . ".";
-					$uploadOk = 1;
-				} else {
-					echo "File is not an image.";
-					$uploadOk = 0;
-				}
-			}
-	
-			// Check if file already exists
-			if (file_exists($target_file)) {
-				echo "Sorry, file already exists.";
-				$uploadOk = 1;
-			}
-	
-			// Check file size
-			if ($_FILES["image"]["size"] > 500000) {
-				echo "Sorry, your file is too large.";
-				$uploadOk = 0;
-			}
-	
-			// Allow certain file formats
-			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-				&& $imageFileType != "gif" ) {
-				echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-				$uploadOk = 0;
-			}
-	
-			// Check if $uploadOk is set to 0 by an error
-			if ($uploadOk == 0){
-				echo "Sorry, your file was not uploaded.";
-				// if everything is ok, try to upload file
-			}else{
-				if (move_uploaded_file($_FILES["image"]["tmp_name"], $n_target_file)) {
-					//echo "The file ". $n_target_file. " has been uploaded.";
-				}else{
-					echo "Sorry, there was an error uploading your file.";
-				}
-			}
-	
-		} */
-
+		public function modifPhotoVoiture($nomPhoto, $ordre, $autoId) {
+			
+			$requete = "UPDATE photo SET nomPhoto = :nomPhoto, ordre = :ordre, autoId = :autoId 
+						WHERE autoId = :autoId AND ordre = :ordre" ;
+			$requetePreparee = $this -> connexion -> prepare($requete);
+            $requetePreparee -> bindParam(":nomPhoto", $nomPhoto);
+            $requetePreparee -> bindParam(":ordre", $ordre);
+            $requetePreparee -> bindParam(":autoId", $autoId);
+            $requetePreparee -> execute();
+		}
 	}
 ?>
