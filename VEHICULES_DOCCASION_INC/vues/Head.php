f391a860df36a4605b2f04a3e24d91a730d344bd
Mathieu D <duchesne.math@gmail.com>
2021-05-12 00:22:42 -0400

    new footer
    refait le footer, celui la marche mieux

--- VEHICULES_DOCCASION_INC/vues/Head.php
+++ VEHICULES_DOCCASION_INC/vues/Head.php
@@ -4,6 +4,7 @@
 	<title>YVMA.inc</title>
 
 	<!-- Styles -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
 	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 	<link rel="stylesheet" href="assets/styles/styles.css">
 	<link rel="stylesheet" href="assets/styles/header.css">
